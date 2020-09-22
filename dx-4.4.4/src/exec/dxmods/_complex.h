a426ee2f2faa4b8cbc17fcf29bde9cf86a4c6d5e
Griff <grifsf@gmail.com>
2020-09-22 16:35:46 -0700

    finalizes header file for warnings in this directory, too many libraries included but it should be fine

--- dx-4.4.4/src/exec/dxmods/_complex.h
+++ dx-4.4.4/src/exec/dxmods/_complex.h
@@ -1,10 +1,11 @@
-#include <dxconfig.h>
+//Contains Declarations for some functions that were giving warnings
+//#include <dxconfig.h>
 #include <dx/dx.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
+//#include <stdlib.h>
+//#include <string.h>
 #include "_compute.h"
-#include "_compoper.h"
+//#include "_compoper.h"
 #include "_compparse.h"
 int _dxfcclex(YYSTYPE *lvalp);
+int _dxfccerror(char *s);
 int _dxfcclexerror();
